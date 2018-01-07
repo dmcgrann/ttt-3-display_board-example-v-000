758ef1bd98a253cf0509d356cbc24d032bb7b35b
David McGrann <dpmcgrann@gmail.com>
2018-01-07 21:39:18 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,5 @@
 def display_board
   cell_row = "   |   |   "
   line = "-----------"
-  puts "Tic Tac Toe Board"
-  puts "#{cell_row}" "\n" "#{line}" "\n" "#{cell_row}" "\n ""#{line}" "\n ""#{cell_row}"
+  puts "#{cell_row}" "\n" "#{line}" "\n" "#{cell_row}" "\n " "#{line}" "\n " "#{cell_row}"
 end
